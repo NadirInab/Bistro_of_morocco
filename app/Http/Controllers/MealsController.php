dd4e40cdd05e33b9b2241257eaab8b362093665c
nadir inab <nadir.inab.dev@gmail.com>
2023-02-13 12:21:07 +0100

    meals functionality is well working

--- app/Http/Controllers/MealsController.php
+++ app/Http/Controllers/MealsController.php
@@ -10,42 +10,77 @@ class MealsController extends Controller
     
     public function index()
     {
-        $data = Meals::all() ;
-        return $data ;
+        $meals = Meals::all() ;
+        // dd($meals) ;
+        return view('welcome')->with('meals', $meals) ;
     }
-
 
     public function create()
     {
-                
+             return view('save') ;   
     }
 
-  
-    public function store(Request $request)
+    public function store(Request $req)
     {
-        //
+        $req->validate([
+            'title' => 'required',
+            'price'=>'required',
+            'description'=>'required',
+            'image'=>'required',
+        ]) ;
+        $data = [
+            'title' => $req->input('title'),
+            'price'=> $req->input('price'),
+            'description' => $req->input('description'),
+            'image' => $req->input('image')
+        ]  ;
+        Meals::create($data) ;
+        return redirect('/dashboard') ;
     }
 
     public function show($id)
     {
-        //
+        $meal = Meals::find($id) ;
+        // dd($meal) ;
+        return view('show')->with('meal', $meal) ;
     }
 
 
     public function edit($id)
     {
-        //
+        $meal = Meals::find($id) ;
+        return view('edit')->with('meal', $meal) ;
     }
 
 
-    public function update(Request $request, $id)
+    public function update(Request $req, $id)
     {
-        //
+        $req->validate([
+            'title' => 'required', 
+            'price'=> 'required',
+            'description' => 'required', 
+            'image'=> 'required'
+        ]) ;
+        $data = [
+            'title'=> $req->input('title'), 
+            'price' => $req->input('price') ,
+            'description'=> $req->input('description'),
+            'image'=> $req->input('image') 
+        ] ;
+        Meals::where('id', $id)->update($data) ;
+        return redirect('/dashboard') ;
     }
 
-  
     public function destroy($id)
     {
-        //
+        $meal = Meals::find($id) ;
+        $meal->delete() ;
+        return redirect('/dashboard') ;
+    }
+
+    public function search(){
+        $search = $_GET['query'] ;
+        $meals = Meals::where('title', 'LIKE', '%'.$search.'%')->get() ;
+        return view('search')->with('meals', $meals) ;
     }
 }
